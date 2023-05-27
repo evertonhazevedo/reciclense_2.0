ce47f0ed03d33c75d78835164d731afec1fe008d
evertonhazevedo <hinckley.everton@gmail.com>
2023-05-27 09:32:48 -0300

    Configuração deploy vercel

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ const rotas = require('./src/routes/routes');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join("https://reciclense-2-0.vercel.app/#", "/")));
+app.use(express.static(path.join(__dirname, "/")));
 
 router.get('/', function (req, res) {
 
-    res.sendFile(path.join("https://reciclense-2-0.vercel.app/#" + '/index.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 
 });
 
