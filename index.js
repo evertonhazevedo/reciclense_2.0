2798f5399ce82f804487fd9aae81bfd5bc4b194e
evertonhazevedo <hinckley.everton@gmail.com>
2023-05-28 16:19:49 -0300

    Back-End da aplicação 06

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 //Configurações
 const express = require('express');
 const app = express();
-const path = require('path');
-const router = express.Router();
+// const path = require('path');
+// const router = express.Router();
 //const cookieParser = require('cooki-parser');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+
+const port = process.eventNames.PORT || 5500;
 
 //Importação das Tabelas
 const models = require('./src/models/models');
@@ -17,19 +19,19 @@ const rotas = require('./src/routes/routes');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, "/")));
+//app.use(express.static(path.join(__dirname, "/")));
 
-router.get('/', function (req, res) {
+app.get('/', function (req, res) {
 
     return res.json('Hello World');
     // res.sendFile(path.join(__dirname + '/index.html'));
 
 });
 
-app.use('/', router);
+//app.use('/', router);
 app.use(rotas);
 app.use(cors());
 //app.use(cookieParser());
 app.use(express.json());
-app.listen(process.env.port || 5500);
+//app.listen(process.env.port || 5500);
 
