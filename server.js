b3a5e57a54516593884a13f6035e8d0dc1cdd0ad
evertonhazevedo <hinckley.everton@gmail.com>
2023-05-29 14:21:28 -0300

    Deploy heroku completo 2

--- server.js
+++ server.js
@@ -13,13 +13,6 @@ const models = require('./src/models/models');
 //Importação das rotas
 const rotas = require('./src/routes/routes');
 
-app.use('/', router);
-app.use(rotas);
-app.use(cors());
-//app.use(cookieParser());
-app.use(express.json());
-app.listen(process.env.port || 5500);
-
 //Configurações
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -28,11 +21,13 @@ app.use(express.static(path.join(__dirname, "/")));
 
 router.get('/', function (req, res) {
 
-    res.setHeader('Access-Control-Allow-Origin', 'https://reciclense.herokuapp.com/');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
     res.sendFile(path.join(__dirname + '/index.html'));
 
 });
+
+app.use('/', router);
+app.use(rotas);
+app.use(cors());
+//app.use(cookieParser());
+app.use(express.json());
+app.listen(process.env.port || 5500);
