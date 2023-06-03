9e4b4fec3e2153793b71f461fe1164a242a5bb00
evertonhazevedo <hinckley.everton@gmail.com>
2023-06-03 09:36:20 -0300

    Deploy valida email

--- src/assets/js/cadastrarUsuario.js
+++ src/assets/js/cadastrarUsuario.js
@@ -24,7 +24,7 @@ document.getElementById('btnCriarConta')
         if (campoEmail.value == "") {
 
             erro.classList.remove("d-none");
-            campo.innerHTML = "Preencha o campo email" // nome do campo que não foi preenchido!
+            campo.innerHTML = "Preencha o campo email!" // nome do campo que não foi preenchido!
             campoEmail.focus();
             campoEmail.classList.add("is-invalid");
 
@@ -32,7 +32,7 @@ document.getElementById('btnCriarConta')
         } else if (campoSenha.value == "") {
 
             erro.classList.remove("d-none");
-            campo.innerHTML = "Preencha o campo senha" // nome do campo que não foi preenchido!
+            campo.innerHTML = "Preencha o campo senha!" // nome do campo que não foi preenchido!
             campoSenha.focus();
             campoSenha.classList.add("is-invalid");
 
@@ -40,7 +40,7 @@ document.getElementById('btnCriarConta')
         } else if (campoConfirmarSenha.value == "") {
 
             erro.classList.remove("d-none");
-            campo.innerHTML = "Preencha o campo confirmar senha" // nome do campo que não foi preenchido!
+            campo.innerHTML = "Preencha o campo confirmar senha!" // nome do campo que não foi preenchido!
             campoConfirmarSenha.focus();
             campoConfirmarSenha.classList.add("is-invalid");
 
@@ -49,18 +49,17 @@ document.getElementById('btnCriarConta')
 
             erro.classList.remove("d-none");
             campoSenha.focus();
-            campo.innerHTML = "As senhas devem ser iguais" // nome do campo que não foi preenchido!
+            campo.innerHTML = "As senhas devem ser iguais!" // nome do campo que não foi preenchido!
             campoConfirmarSenha.focus();
             campoSenha.classList.add("is-invalid");
             campoConfirmarSenha.classList.add("is-invalid");
 
         } else if (!validarEmail(email)) {
 
-            Swal.fire({
-                icon: 'error',
-                title: 'Email inválido!',
-                text: 'Favor informar um email válido.'
-            });
+            erro.classList.remove("d-none");
+            campo.innerHTML = "Email inválido!" // nome do campo que não foi preenchido!
+            campoEmail.focus();
+            campoEmail.classList.add("is-invalid");
 
         } else {
 
